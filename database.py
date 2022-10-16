ad2d9a561dbc4ae2c761cd0b6a04c613353bb310
Lucas Santos Silva <luss1010@gmail.com>
2022-10-16 17:45:12 -0300

    subindo api e banco de dados

---
+++ database.py
@@ -0,0 +1,46 @@
+from peewee import *
+from datetime import datetime
+import os
+
+user = os.getenv("db_user")
+password = os.getenv("db_password")
+host = os.getenv("db_host")
+port = int(os.getenv("db_port"))
+pg_db = PostgresqlDatabase("", user=user, password=password,
+                           host=host, port=port)
+
+
+class BaseModel(Model):
+    time_stamp = DateTimeField(default=datetime.now, null=True)
+
+    class Meta:
+        database = pg_db
+
+
+class Products(BaseModel):
+    name = TextField()
+    code_ncm = IntegerField()
+    description = TextField()
+    number_units = FloatField()
+    type_units = TextField()
+    value_units = FloatField()
+    amount = FloatField(default=(number_units*value_units))
+
+
+class Orders(BaseModel):
+    pass
+
+
+class ListProducts(BaseModel):
+    product_id = ForeignKeyField(Products, backref="ListProducts")
+    order_id = ForeignKeyField(Orders, backref="ListProducts")
+    number_units = FloatField()
+    value_units = FloatField()
+    amount = FloatField(default=(number_units * value_units))
+
+
+pg_db.create_tables([Products, Orders, ListProducts])
+
+
+if __name__ == "__main__":
+    print()
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    username: str
+    password: str
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Rota Raiz
+@app.get("/")
+def root():
+    return {"status": "ok"}
